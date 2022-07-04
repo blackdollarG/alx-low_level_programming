337d9e2cfa043292734f6a2d73ae6a2626531834
blackdollarG <carolinewartz@gmail.com>
2022-07-04 11:33:00 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,15 +10,13 @@ int main(void)
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-	{
-		putchar(letter);
-	}
-	for (letter = 'A'; letter <= 'Z'; letter++)
+	if (letter != 'e' && letter != 'q')
 	{
 		putchar(letter);
 	}
 	{
 		putchar('\n');
 	}
+
 	return (0);
 }
