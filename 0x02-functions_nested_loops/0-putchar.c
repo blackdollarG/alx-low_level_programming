4f6ac8b7c574bf38bb7d96289bb745559863f58a
blackdollarG <carolinewartz@gmail.com>
2022-07-05 09:12:43 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	char dollar[9] = "_putchar";
+	char joy[9] = "_putchar";
 	int i;
 
 	for (i = 0; i < 8; i = i + 1)
 	{
-		_putchar(dollar[i]);
+		_putchar(joy[i]);
 	}
 	_putchar('\n');
 	return (0);
